8364ef181af91f21d302a35ab39239b0fea97034
John-Alz <jairoanngelll@gmail.com>
2022-09-02 08:49:27 -0500

    update(client/src/components/favoritos): update favoritosUpdate

--- client/src/components/TarjetaFav.jsx
+++ client/src/components/TarjetaFav.jsx
@@ -15,7 +15,7 @@ const FavoriteCard = ({id, name, price, img}) => {
     const favoriteId = favorito.map((p) => p.id);
 
     const handleAddCart = () => {
-		if (Boolean(favorito.find((p) => p.id === id))) return;
+		if (Boolean(cart.find((p) => p.id === id))) return;
 		else {
 			dispatch(
 				addProduct({
@@ -42,14 +42,14 @@ const FavoriteCard = ({id, name, price, img}) => {
 
 	return (
 		<div className="flex justify-center  p-2">
-			<div className="flex flex-col md:flex-row max-w-prose rounded-lg bg-white shadow-lg">
+			<div className="flex flex-col md:flex-row md:max-w-6xl rounded-lg bg-white shadow-lg">
 				<img
 					className="bg-pink-700 w-20 h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
 					src={img}
 					alt=""
 				/>
 				<div className="p-6 flex flex-col justify-start">
-					<h5 className="text-gray-900 text-xl font-medium mb-2">{name}</h5>
+					<h5 className="text-gray-900 text-xl font-medium mb-2">{name.slice(0, 45)}...</h5>
 					<p className="text-gray-700 text-base mb-4">
 						This is a special product of our RGBtech page for all our customers
 					</p>
@@ -57,27 +57,25 @@ const FavoriteCard = ({id, name, price, img}) => {
 						<p className="text-gray-600 text-xl font-bold">
 							{price} 
 						</p>
-                        {/* <div className="mr-96">
+                        <div className="mr-96">
                             <BsCurrencyDollar/>
-                        </div> */}
+                        </div>
 					</div>
+
                     <div className="pt-4">
+
                     <button
 						className="px-4 h-8 rounded font-semibold border border-blue-400 hover:scale-95 text-slate-900"
-						type="button"
                         onClick={handleAddCart}
 						>
 						Add to cart
-						</button>
-                        {
-                            favoriteId && favoriteId.includes(id) ? (
+					</button>
+                        
                         <button 
                         onClick={handleDeleteCartFav}
                         className="ml-12 scale-110 mr-2 px-2.5 py-0.5">
                         <GiTechnoHeart/>
-                        </button>) : <span><small>fallo</small></span>
-                        }
-                        
+                        </button>
                         
                     </div>
 				</div>
